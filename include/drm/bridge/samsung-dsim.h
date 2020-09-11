74274f642f0610b23e2ca3ed97f1a563d53e9cea
Michael Tretter <m.tretter@pengutronix.de>
2020-09-11 15:54:13 +0200

    drm/exynos: move bridge driver to bridges
    As the driver is not platform dependent anymore, move it to the drm
    bridge driver directory.
    
    Signed-off-by: Michael Tretter <m.tretter@pengutronix.de>

--- include/drm/bridge/samsung-dsim.h
+++ include/drm/bridge/samsung-dsim.h
@@ -3,7 +3,7 @@
 #define __EXYNOS_DRM_DSI__
 
 struct drm_encoder;
-struct exynos_dsi;
+struct samsung_dsim;
 struct platform_device;
 struct mipi_dsi_device;
 
@@ -12,13 +12,13 @@ enum exynos_reg_offset {
 	EXYNOS5433_REG_OFS
 };
 
-struct exynos_dsi *exynos_dsi_probe(struct platform_device *pdev);
-void exynos_dsi_remove(struct exynos_dsi *dsi);
-int exynos_dsi_bind(struct exynos_dsi *dsi, struct drm_encoder *encoder);
-void exynos_dsi_unbind(struct exynos_dsi *dsi);
+struct samsung_dsim *samsung_dsim_probe(struct platform_device *pdev);
+void samsung_dsim_remove(struct samsung_dsim *dsi);
+int samsung_dsim_bind(struct samsung_dsim *dsi, struct drm_encoder *encoder);
+void samsung_dsim_unbind(struct samsung_dsim *dsi);
 
-int exynos_dsi_suspend(struct exynos_dsi *dsi);
-int exynos_dsi_resume(struct exynos_dsi *dsi);
+int samsung_dsim_suspend(struct samsung_dsim *dsi);
+int samsung_dsim_resume(struct samsung_dsim *dsi);
 
 enum reg_value_idx {
 	RESET_TYPE,
@@ -42,13 +42,13 @@ enum reg_value_idx {
 #define DSIM_FUNCRST			(1 << 16)
 #define DSIM_SWRST			(1 << 0)
 
-struct exynos_dsi_host_ops {
+struct samsung_dsim_host_ops {
 	int (*attach)(struct device *dev, struct mipi_dsi_device *device);
 	int (*detach)(struct device *dev, struct mipi_dsi_device *device);
 	void (*te_handler)(struct device *dev);
 };
 
-struct exynos_dsi_driver_data {
+struct samsung_dsim_driver_data {
 	enum exynos_reg_offset reg_ofs;
 	unsigned int plltmr_reg;
 	unsigned int has_freqband:1;
@@ -58,7 +58,7 @@ struct exynos_dsi_driver_data {
 	unsigned int wait_for_reset;
 	unsigned int num_bits_resol;
 	const unsigned int *reg_values;
-	const struct exynos_dsi_host_ops *host_ops;
+	const struct samsung_dsim_host_ops *host_ops;
 };
 
 #endif /* __EXYNOS_DRM_DSI__ */
