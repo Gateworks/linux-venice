207ed58bc77eedacbd7e114557f133ef7a9e1dad
Marek Vasut <marex@denx.de>
2020-10-04 00:45:53 +0200

    dt-bindings: reset: imx8mm: Add media blk_ctl reset IDs
    These will be used by the imx8mm for blk_ctl driver.
    
    Signed-off-by: Marek Vasut <marex@denx.de>
    Cc: Abel Vesa <abel.vesa@nxp.com>
    Cc: Dong Aisheng <aisheng.dong@nxp.com>
    Cc: Fabio Estevam <festevam@gmail.com>
    Cc: Guido Günther <agx@sigxcpu.org>
    Cc: Lucas Stach <l.stach@pengutronix.de>
    Cc: Rob Herring <robh+dt@kernel.org>
    Cc: Shawn Guo <shawnguo@kernel.org>
    Cc: NXP Linux Team <linux-imx@nxp.com>
    Cc: devicetree@vger.kernel.org
    Acked-by: Rob Herring <robh@kernel.org>

---
+++ include/dt-bindings/reset/imx8mm-reset.h
@@ -0,0 +1,14 @@
+/* SPDX-License-Identifier: GPL-2.0-only */
+/*
+ * Copyright (C) 2020 Marek Vasut <marex@denx.de>
+ */
+
+#ifndef DT_BINDING_RESET_IMX8MM_H
+#define DT_BINDING_RESET_IMX8MM_H
+
+#define IMX8MM_MEDIA_BLK_CTL_RESET_MIPI_DSI_I_PRESET	0
+#define IMX8MM_MEDIA_BLK_CTL_RESET_MIPI_M_RESET		1
+
+#define IMX8MM_MEDIA_BLK_CTL_RESET_NUM			2
+
+#endif
