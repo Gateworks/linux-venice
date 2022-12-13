0a198e6eadf5ea63010d1ff558893dce010cce2c
Jagan Teki <jagan@amarulasolutions.com>
2022-12-13 12:23:07 +0530

    drm: exynos: dsi: Add input_bus_flags
    LCDIF-DSIM glue logic inverts the HS/VS/DE signals and expecting
    the i.MX8M Mini/Nano DSI host to add additional Data Enable signal
    active low (DE_LOW). This makes the valid data transfer on each
    horizontal line.
    
    So, add additional bus flags DE_LOW setting via input_bus_flags
    for i.MX8M Mini/Nano platforms.
    
    Tested-by: Marek Szyprowski <m.szyprowski@samsung.com>
    Reviewed-by: Marek Vasut <marex@denx.de>
    Reviewed-by: Frieder Schrempf <frieder.schrempf@kontron.de>
    Suggested-by: Marek Vasut <marex@denx.de>
    Signed-off-by: Jagan Teki <jagan@amarulasolutions.com>

--- drivers/gpu/drm/exynos/exynos_drm_dsi.c
+++ drivers/gpu/drm/exynos/exynos_drm_dsi.c
@@ -1715,6 +1715,10 @@ static const struct component_ops exynos_dsi_component_ops = {
 	.unbind	= exynos_dsi_unbind,
 };
 
+static const struct drm_bridge_timings dsim_bridge_timings_de_low = {
+	.input_bus_flags = DRM_BUS_FLAG_DE_LOW,
+};
+
 static int exynos_dsi_probe(struct platform_device *pdev)
 {
 	struct device *dev = &pdev->dev;
@@ -1799,6 +1803,10 @@ static int exynos_dsi_probe(struct platform_device *pdev)
 	dsi->bridge.funcs = &exynos_dsi_bridge_funcs;
 	dsi->bridge.of_node = dev->of_node;
 	dsi->bridge.type = DRM_MODE_CONNECTOR_DSI;
+
+	/* DE_LOW: i.MX8M Mini/Nano LCDIF-DSIM glue logic inverts HS/VS/DE */
+	if (dsi->plat_data->hw_type == DSIM_TYPE_IMX8MM)
+		dsi->bridge.timings = &dsim_bridge_timings_de_low;
 
 	ret = component_add(dev, &exynos_dsi_component_ops);
 	if (ret)
